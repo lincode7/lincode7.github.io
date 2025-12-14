144e08cc6a69864b43b4d8cb86c1205b214a0c1f
licode7 <zm1292017430@163.com>
2025-12-14 01:45:40 +0000

    fix: 解决marked插件异常

--- src/utils/mardownPhaser.ts
+++ src/utils/mardownPhaser.ts
@@ -2,7 +2,8 @@ import hljs from "highlight.js";
 import { marked } from "marked";
 import { markedHighlight } from "marked-highlight";
 import markedKatex from "marked-katex-extension";
-const extendedTables = require("marked-extended-tables");
+// @ts-ignore
+import extendedTables from "marked-extended-tables";
 
 const myMarked = marked
   .use({
