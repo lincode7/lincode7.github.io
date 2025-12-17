f980e8ebef4ce2d2544f50879f7e6ab0539c334b
licode7 <zm1292017430@163.com>
2025-12-17 14:07:33 +0000

    Updates

---
+++ assets/rpg-D6lCHInC.js
@@ -0,0 +1,29 @@
+const n=`[
+  {
+    "type": "game",
+    "title": "贪婪之秋(Greed fall)",
+    "platform": ["PC"],
+    "tags": ["RPG", "剧情", "氛围"],
+    "status": "playing",
+    "hours": 29.1,
+    "date": "2025-11-26",
+    "rating": 7,
+    "description": "意料之外的还不错，剧情、战斗都各有特点。\\n剧情：8。中世纪探索新大陆，作为外交官，缓解盟国与本地土著之间的紧张关系。\\n战斗：8。4个流派，枪（配合陷阱加点，点出投资药剂后，大大降低了游戏难度），剑（单手剑与双手剑），钝器（钉锤、斧头等，整个流程没用过，除了后期有一把大太刀，前期武器不好看），戒指（法师，没用过，队伍里有一个主教，不太需要，戒指本身的攻击方式比较单调，不知道有没有法术）。\\n3个构筑系统：技能解锁与强化、人物属性加点（装备最低要求）、生活加点（支撑武器制作、强化、任务对话解锁、野外捷径）",
+    "image": "https://tse4-mm.cn.bing.net/th/id/OIP-C.FNBgPU49Ib7b8bxxS-4sWQHaEK?w=280&h=180&c=7&r=0&o=7&cb=ucfimg2&dpr=1.1&pid=1.7&rm=3&ucfimg=1",
+    "link": "https://store.steampowered.com/app/606880"
+  },
+  {
+    "type": "game",
+    "title": "33号远征队(Expedition 33)",
+    "platform": ["PC"],
+    "tags": ["RPG", "剧情", "回合制"],
+    "status": "playing",
+    "hours": 46.5,
+    "date": "2025-11-26",
+    "rating": 8,
+    "description": "独居创意的会和战斗机制，还不错的剧情，丰富的人物塑造与地图探索。",
+    "image": "https://tse1-mm.cn.bing.net/th?id=OIF-C.16D0wavV5CTC7o%2b5G3%2fKwg&w=272&h=180&c=7&r=0&o=7&cb=ucfimg2&dpr=1.1&pid=1.7&rm=3&ucfimg=1",
+    "link": "https://store.steampowered.com/agecheck/app/1903340"
+  }
+]
+`;export{n as default};
